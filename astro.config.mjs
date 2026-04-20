8640091b6308961fb9775cd15c8bfa62a2d149a8
droidmaximus <bhojanapalliavinash@gmail.com>
2026-04-19 23:12:34 -0400

    fixed config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import edgeoneAdapter from "@edgeone/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: edgeoneAdapter(),
-  // Set to your production URL (GitHub Pages, Vercel, Netlify, etc.).
-  site: "https://droidmaximus.github.io",
+  site: "https://avinash.social",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
